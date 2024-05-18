66603f52f4504c1ce20ec920897fa1ef6253cf1f
vadimkholodilo <vadimkholodilo@gmail.com>
2024-05-18 14:44:10 +0300

    Finish rpc contract

--- Nl2Sql.Api/Program.cs
+++ Nl2Sql.Api/Program.cs
@@ -5,8 +5,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddGrpcReflection();
 
 var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.MapGrpcReflectionService();
+}
+
 app.MapGrpcService<TranslatorService>();
 // Configure the HTTP request pipeline.
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
